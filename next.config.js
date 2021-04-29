3051cf8922919da83562819a2436264a63c2a861
akbarkhangit <akbarkhanprofessional@gmail.com>
2021-04-29 22:52:43 +0530

    next config file updated

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-// const { PHASE_PRODUCTION_SERVER } =
-//   process.env.NODE_ENV === 'development'
-//     ? {}
-//     : !process.env.NOW_REGION
-//       ? require('next/constants')
-//       : require('next-server/constants');
+const { PHASE_PRODUCTION_SERVER } =
+    process.env.NODE_ENV === 'development'
+        ? {}
+        : !process.env.NOW_REGION
+            ? require('next/constants')
+            : require('next-server/constants');
 
 module.exports = (phase, { defaultConfig }) => {
     if (phase === PHASE_PRODUCTION_SERVER) {
