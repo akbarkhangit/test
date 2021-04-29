8fbd70314b7007dcf270d7679ca693db56cb282d
akbarkhangit <akbarkhanprofessional@gmail.com>
2021-04-29 23:10:03 +0530

    next.config updated

--- next.config.js
+++ next.config.js
@@ -1,22 +1,26 @@
-// const { PHASE_PRODUCTION_SERVER } =
-//     process.env.NODE_ENV === 'development'
-//         ? {}
-//         : !process.env.NOW_REGION
-//             ? require('next/constants')
-//             : require('next-server/constants');
+const { PHASE_PRODUCTION_SERVER } =
+    process.env.NODE_ENV === 'development'
+        ? {}
+        : !process.env.NOW_REGION
+            ? require('next/constants')
+            : require('next-server/constants');
 
-// module.exports = (phase, { defaultConfig }) => {
-//     if (phase === PHASE_PRODUCTION_SERVER) {
-//         // Config used to run in production.
-//         return {};
-//     }
-// }
+module.exports = (phase, { defaultConfig }) => {
+    if (phase === PHASE_PRODUCTION_SERVER) {
+        // Config used to run in production.
+        return {
+            target: 'serverless'
+        };
+    }
 
-    //   // Put the require call here.
-    //   const withCSS = require('@zeit/next-css');
+    // ✅ Put the require call here.
+    const withCSS = require('@zeit/next-sass');
 
-    //   return withCSS();
-    // };
+    module.exports = withCSS({
+        target: 'serverless'
+    });
+};
+
 
     // next.config.js
     // const withCSS = require('@zeit/next-css');
