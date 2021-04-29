f1cf83535f272d32088dc34c55499c21ed7347e9
akbarkhangit <akbarkhanprofessional@gmail.com>
2021-04-29 22:59:04 +0530

    next config commented

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
-const { PHASE_PRODUCTION_SERVER } =
-    process.env.NODE_ENV === 'development'
-        ? {}
-        : !process.env.NOW_REGION
-            ? require('next/constants')
-            : require('next-server/constants');
+// const { PHASE_PRODUCTION_SERVER } =
+//     process.env.NODE_ENV === 'development'
+//         ? {}
+//         : !process.env.NOW_REGION
+//             ? require('next/constants')
+//             : require('next-server/constants');
 
-module.exports = (phase, { defaultConfig }) => {
-    if (phase === PHASE_PRODUCTION_SERVER) {
-        // Config used to run in production.
-        return {};
-    }
-}
+// module.exports = (phase, { defaultConfig }) => {
+//     if (phase === PHASE_PRODUCTION_SERVER) {
+//         // Config used to run in production.
+//         return {};
+//     }
+// }
 
     //   // Put the require call here.
     //   const withCSS = require('@zeit/next-css');
